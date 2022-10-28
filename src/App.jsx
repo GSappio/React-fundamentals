b7444f2f94e60403990540788f71830d1f6df70a
Gabriel Sappio <100438648+GSappio@users.noreply.github.com>
2022-10-28 09:42:31 -0300

    Repetition: Transforming a students Array into JSX

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 
 import Card from './components/layout/Card';
 
+import ListaAluno from './components/repeticao/ListaAluno';
 import Familia from './components/basicos/Familia';
 import FamiliaMembro from './components/basicos/FamiliaMembro';
 import Aleatorio from './components/basicos/Aleatorio';
@@ -15,14 +16,16 @@ export default () => (
       <h1>Fundamentos React</h1> 
     <div className="Cards">
 
+        <Card titulo="#06 - Repetição" color="#FF4C65">
+           <ListaAluno></ListaAluno>
+        </Card>
+
         <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
             <Familia sobrenome="Silva">
             <FamiliaMembro nome="Pedro"/>
             <FamiliaMembro nome="Ana"/>
-            <FamiliaMembro nome="Gustavo"/>
-                
+            <FamiliaMembro nome="Gustavo"/>    
             </Familia>
-            
         </Card>
 
         <Card titulo="#04 - Desafio aleatório" color="#FA6900">
