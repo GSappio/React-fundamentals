1b5dedc32773819735fc7121c55106a0cc572e68
Gabriel Sappio <100438648+GSappio@users.noreply.github.com>
2022-10-28 21:05:28 -0300

    Creating a counter #01

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 
 import Card from './components/layout/Card';
 
+import Contador from './components/contador/Contador';
 import Input from './components/formulario/Input';
 import IndiretaPai from './components/comunicacao/IndiretaPai';
 import DiretaPai from './components/comunicacao/DiretaPai';
@@ -23,6 +24,10 @@ export default () => (
 
 
     <div className="Cards">
+        <Card titulo="#12 - Contador" color="#424242">
+           <Contador numeroInicial={10}></Contador>
+        </Card>
+
         <Card titulo="#11 - Componente Controlado" color="#E45F56">
            <Input></Input>
         </Card>
