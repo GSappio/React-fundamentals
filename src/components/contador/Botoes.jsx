38bf2aef0db4e0991e0c060e40468eccc22b986a
Gabriel Sappio <100438648+GSappio@users.noreply.github.com>
2022-10-31 08:27:54 -0300

    Creating a counter #02

---
+++ src/components/contador/Botoes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default props => {
+    return (
+       
+        <div>
+
+                <button onClick={props.setInc}>+</button> 
+                <button onClick={props.setDec}>-</button>
+
+        </div>
+
+    )
+
+}
